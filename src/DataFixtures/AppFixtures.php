228ad76903c5eb1115096e839c9b4fca9859cbcd
Diego Ply <ply.dgo@gmail.com>
2024-01-16 18:56:40 +0100

    nueuvième commit

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    public function __construct(UserPasswordHasherInterface $passwordHasher){
+        $this->passwordHasher = $passwordHasher;
+        
+    }
+
+
+
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User($this->passwordHasher);
+        $user->setUsername("N0tChris")->setPassword("123");
+
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
