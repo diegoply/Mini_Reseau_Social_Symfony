29a2dfb467038e6cdfdf0e76b7a5549ac1dbfe20
Diego Ply <ply.dgo@gmail.com>
2024-01-16 11:24:57 +0100

    septième commit

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Post;
 use App\Form\PostType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,15 +14,22 @@ class PostController extends AbstractController
 {
 
     #[Route('/')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
-        return $this->render('post/index.html.twig', 
+      
+
+            $repository = $doctrine->getRepository(Post::class);
+            $posts = $repository->findAll(); // SELECT * FROM 'post';
+            return $this->render('post/index.html.twig', [
+                "posts" => $posts
+            ]);
+        
             
-        );
+        
     }
 
     #[Route('/post/new')]
-    public function create(Request $request) : Response 
+    public function create(Request $request, ManagerRegistry $doctrine) : Response 
     {
         $post = new Post();
         $form = $this->createForm(PostType::class, $post);
@@ -29,6 +37,11 @@ class PostController extends AbstractController
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+
+            $em = $doctrine->getManager();
+            $em -> persist($post);
+            $em ->flush();
+
             dump($post);
         }
 
